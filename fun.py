d72511eaa10dadf66c29ffbc37fe177345d126d7
Asenath0 <60759090+Asenath0@users.noreply.github.com>
2020-03-06 18:12:45 +0100

    Update fun.py

--- fun.py
+++ fun.py
@@ -16,8 +16,9 @@ typ użytkownika 2
 def log():
     plik = open("logins and passwords.txt", "r").read()
     lines = plik.split("\n")
-    login = str(input("Podaj login: "))
-    password = str(input("Podaj haslo: "))
+    login = input("Podaj login: ")
+    password = input("Podaj haslo: ")
+    user = 0
 
     for i in range(len(lines)):
         if (login == lines[i]):
@@ -30,6 +31,7 @@ def log():
                 user_ = open("user.txt", "w")
                 user_.write(user)
                 user_.close()
+                user = 1
 
 
     if (user == 0):
@@ -40,16 +42,14 @@ def log():
 #możliwe wyniki: student , teacher , admin
 def user_type():
     plik = open("user.txt", "r").read()
-    pliks = plik.split("\n")
-    for i in pliks:
-        return (i)
+    return (plik)
 
 #Funkcja zapisuje login, hasło, imię i nazwisko nowego ucznia do pliku logins and passwords.txt a także jego godność do lokalnej tablicy w pliku local.py
 def add_student():
     logowanie = open("logins and passwords.txt", "a")
-    name = str(input("Podaj imię i nazwisko ucznia: "))
-    login = str(input("Podaj login dla ucznia: "))
-    password = str(input("Podaj haslo dla ucznia: "))
+    name = input("Podaj imię i nazwisko ucznia: ")
+    login = input("Podaj login dla ucznia: ")
+    password = input("Podaj haslo dla ucznia: ")
 
     logowanie.write(login)
     logowanie.write("\n")
@@ -59,18 +59,19 @@ def add_student():
     logowanie.write("\n")
     logowanie.write("student")
     logowanie.write("\n")
+    logowanie.write("\n")
     logowanie.close()
 
     source = open("local.py").readlines()
     goal = open("local.py", "w")
     for i in source:
-        goal.write(i.replace("]", " ,"))
+        goal.write(i.replace("]", " "))
     goal.close()
     source = open("local.py", "a")
     source.write("'")
     source.write(name)
     source.write("'")
-    source.write("\n")
+    source.write(",")
     source.write("]")
 
 
